32b6b347dd453826cf65b93e4ff527427e4f4197
dauskkbd <dauskkbd@gmail.com>
2024-02-05 17:24:00 +0800

    updating head.blade.php

--- rollnplay/resources/views/layouts/head.blade.php
+++ rollnplay/resources/views/layouts/head.blade.php
@@ -1,14 +1,22 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <!-- Icons -->
+
 <link rel="icon" type="image/x-icon" href="/images/Roll_fav.png">
 <!-- Font Awesome -->
+
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
+
 <!-- Google Fonts -->
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Londrina+Solid:wght@100;300;400;900&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
+
 <!-- MDB -->
 <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.1.0/mdb.min.css" rel="stylesheet" />
 <link rel="stylesheet" href="/css/fonts.css" />
+
+<!--bootstrap 5, link removed to avoid contradiction with MDB-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
