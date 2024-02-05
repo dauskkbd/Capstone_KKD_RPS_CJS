05ef6708716d21b919cc0093143ea4529ee51440
dauskkbd <dauskkbd@gmail.com>
2024-02-05 14:14:25 +0800

    updating home page and head blade with mdb and bootstrap 5

--- rollnplay/resources/views/home.blade.php
+++ rollnplay/resources/views/home.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+   
     @include('layouts/head')
     <style>
         body{
@@ -9,7 +10,7 @@
 
         /* hero banner section */
         #hero_banner_section{
-            margin-top: 6%;
+            margin-top: 10%;
         }
 
         #banner_header, #shop_by_category_header {
@@ -238,73 +239,12 @@
             background-color: rgba(0, 128, 0, 0.8);
         }
     </style>
-    <script>
-         $(document).ready(function(){
-
-            // on click event function that changes pictures whenever one of the four images were clicked
-            $(".image-1").click(function(){
-                $(".main-image").attr("src", $(this).attr("src"));
-            });
-            $(".image-2").click(function(){
-                $(".main-image").attr("src", $(this).attr("src"));
-            });
-            $(".image-3").click(function(){
-                $(".main-image").attr("src", $(this).attr("src"));
-            });
-            $(".image-4").click(function(){
-                $(".main-image").attr("src", $(this).attr("src"));
-            });
-
-            //prevents user from typing a number larger than 9
-            $('.order-number').on('input', function() {
-           let value = parseInt($(this).val(), 10);
-           let maxValue = 9;
-            if (value > maxValue) {
-            $(this).val(maxValue);
-            }
-            });
-
-            //ensures that the user only types numbers and not letters
-            $('.order-number').on('keypress', function(event) {
-            let allowedKeys = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 8];
-            let keyCode = event.which;
-            if (!(allowedKeys.includes(keyCode))) {
-            event.preventDefault();
-            }
-            });
-
-            //ensures that when the input form is out of focus, the value zero will be displayed again
-            $('.order-number').on('blur', function() {
-            if ($(this).val().trim() === '') {
-            $(this).val('0');
-            }
-            });
-
-            //click event function that decreases the value of the input form
-            $('.decrease-btn').click(function() {
-            let inputField = $(this).siblings('.order-number');
-            let value = parseInt(inputField.val(), 10);
-            if (value > 0) {
-            inputField.val(value - 1);
-            }
-            });
-
-            //click event function that increases the value of the input form
-            $('.increase-btn').click(function() {
-            let inputField = $(this).siblings('.order-number');
-            let value = parseInt(inputField.val(), 10);
-            let maxValue = 9;
-            if (value < maxValue) {
-            inputField.val(value + 1);
-            }
-            });
-        });
-    </script>
+    <script src="js/home.js"></script>
     <title>RollNPlay-Home</title>
 </head>
 <body>
-    @include('layouts/navbar')
-
+    
+@include('layouts/navbar')
     {{-- hero banner section --}}
 
     <div class="container-fluid" id="hero_banner_section">
@@ -312,7 +252,7 @@
             <h1>Ready to Roll&Play?</h1>
         </div>
         <div class="row">
-            <div class="col-lg-6 col-md-12 col-12 carousel slide" data-bs-ride="carousel" data-bs-interval="5000">
+            <div class="col-lg-6 col-md-12 col-12 carousel slide" data-mdb-ride="carousel" data-mdb-carousel-init data-mdb-interval="5000">
                 <div class="carousel-inner">
                     <div class="carousel-item active">
                         <a href=""><img src="/images/septima_resize.jpg" alt="blank image" class="img-fluid hero-banner"></a>
@@ -495,6 +435,9 @@
             </div>
         </div>
     </div>
-    @include('layouts/footer')
+   @include('layouts/script')
+   @include('layouts/footer')
 </body>
+
+
 </html>
