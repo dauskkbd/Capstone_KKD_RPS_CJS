0cfe4d49ec10319fd2654e6be4ee566a764976aa
dauskkbd <dauskkbd@gmail.com>
2024-02-06 10:15:53 +0800

    updating files for capstone project

---
+++ rollnplay/app/Http/Controllers/ProductController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+
+use App\Models\Product;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\DB;
+
+class ProductController extends Controller
+{
+}
