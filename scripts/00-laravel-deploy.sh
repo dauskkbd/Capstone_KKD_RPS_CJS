1f83b8051dc259076ed0508942d90dbb0577be02
dauskkbd <dauskkbd@gmail.com>
2024-02-12 02:22:31 +0800

    changed code

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -13,10 +13,4 @@ echo "Running migrations..."
 php artisan migrate --force
 
 echo "Running seeders"
-php artisan db:seed --class=UsersTableSeeder --force
-php artisan db:seed --class=CartsTableSeeder --force
-php artisan db:seed --class=OrdersTableSeeder --force
-php artisan db:seed --class=OrderProductTableSeeder --force
-php artisan db:seed --class=ProductsTableSeeder --force
-php artisan db:seed --class=MigrationsTableSeeder --force
-php artisan db:seed --class=UsersCartsTableSeeder --force
+php artisan db:seed --force
