3918d1cefe958c7236a4f3cb539b0111885ecb6a
dauskkbd <dauskkbd@gmail.com>
2024-02-29 20:07:17 +0800

    replacing existing codes

--- resources/views/admin_dashboard.blade.php
+++ resources/views/admin_dashboard.blade.php
@@ -9,16 +9,16 @@
 <body>
     @include("layouts/navbar_admin")
     <div class="container admin-dashboard-container">
-        <div class="row">
+        {{-- <div class="row">
             <h1>Admin Dashboard</h1>
             <div class="col-lg-6">
                 <canvas id="chart1"></canvas>
             </div>
-            {{-- <div class="col-lg-6">
+            <div class="col-lg-6">
                 <canvas id="chart2"></canvas>
-            </div> --}}
+            </div>
         </div>
-    </div>
+    </div> --}}
 
     <div class="container admin-card-container">
         <div class="row">
@@ -47,48 +47,47 @@
     </div>
     @include("layouts/footer")
     @include("layouts/script")
-    <script>
-        var ctx = document.getElementById('chart1').getContext('2d');
-        function getColorFromString(str) {
-        var hash = 0;
-        for (var i = 0; i < str.length; i++) {
-            hash = str.charCodeAt(i) + ((hash << 5) - hash);
-        }
-        var color = '#';
-        for (var i = 0; i < 3; i++) {
-            var value = (hash >> (i * 8)) & 0xFF;
-            color += ('00' + value.toString(16)).substr(-2);
-        }
-        return 'rgba(' + parseInt(color.substr(1, 2), 16) + ',' + parseInt(color.substr(3, 2), 16) + ',' + parseInt(color.substr(5, 2), 16) + ', 1)';
-        }
-    
-        var myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: @json($data['labels']),
-            datasets: [{
-                label: 'Profit',
-                data: @json($data['data']),
-                backgroundColor: function(context) {
-                    var date = context.chart.data.labels[context.dataIndex];
-                    return getColorFromString(date); // Generate color based on the date
-                },
-                borderColor: 'rgba(75, 192, 192, 1)',
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
-        }
-        });
-    </script>
 </body>
 
+{{-- <script>
+    var ctx = document.getElementById('chart1').getContext('2d');
+    function getColorFromString(str) {
+    var hash = 0;
+    for (var i = 0; i < str.length; i++) {
+        hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    var color = '#';
+    for (var i = 0; i < 3; i++) {
+        var value = (hash >> (i * 8)) & 0xFF;
+        color += ('00' + value.toString(16)).substr(-2);
+    }
+    return 'rgba(' + parseInt(color.substr(1, 2), 16) + ',' + parseInt(color.substr(3, 2), 16) + ',' + parseInt(color.substr(5, 2), 16) + ', 1)';
+    }
 
+    var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: @json($data['labels']),
+        datasets: [{
+            label: 'Profit',
+            data: @json($data['data']),
+            backgroundColor: function(context) {
+                var date = context.chart.data.labels[context.dataIndex];
+                return getColorFromString(date); // Generate color based on the date
+            },
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+    });
+</script> --}}
 {{-- <script>
      var ctx = document.getElementById('chart2').getContext('2d');
         var myChart = new Chart(ctx, {
